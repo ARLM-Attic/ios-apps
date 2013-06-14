1d8689ddcd7ed0daf53389775c3b1667b565e083
SND\larsben_cp <SND\larsben_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2013-06-14 02:05:47 +0000

    Prototype
    git-svn-id: https://iosapps.svn.codeplex.com/svn@84053 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ FlashCards/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  FlashCards
+//
+//  Created by mac on 3/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
